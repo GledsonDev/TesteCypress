3c8d387157ad3d90c4c7cd9dafc2dca3dc431dab
Gledson <gledsonst366@gmail.com>
2024-06-20 02:40:19 -0300

    tem q consertar

--- cypress/e2e/amazon.cy.js
+++ cypress/e2e/amazon.cy.js
@@ -1,36 +1,36 @@
 const teste = "https://www.amazon.com.br/?tag=msndesktopabk-20&ref=pd_sl_7to86bd2ph_e&adgrpid=1141293728081284&hvadid=71331080112049&hvnetw=o&hvqmt=e&hvbmt=be&hvdev=c&hvlocint=&hvlocphy=147001&hvtargid=kwd-71331371436168:loc-20&hydadcr=26346_11803856&msclkid=5bfd8417ecf7144efb22e2678cd76ec9";
 
-const email = "teste.ficr@gmail.com";
+const email = "testeficr23@gmail.com";
 const password = "testedoteste";
 
 describe("Teste compra na Amazon", () => {
-  it("Deverá fazer um fluxo de compra e desistência de compra na Amazon", () => {
+  it("Deverá fazer um fluxo de compra, fazer o login e chegada até o preenchimento dos dados para finalização da compra", () => {
     // IR PARA A AMAZON
     cy.log('Ir para o site da Amazon.');
     cy.visit(teste);
 
-    // ADICIONAR UM ITEM AO CARRINHO (mudar item)
+    // ADICIONAR UM ITEM AO CARRINHO
     cy.log('Adicionar um item ao carrinho.');
     cy.get('#twotabsearchtextbox').type('Caneta {enter}');
     cy.get('.s-main-slot .s-result-item[data-component-type="s-search-result"]').first().find('h2 a').click();
-    cy.get('#add-to-cart-button').click({ force: true });
+    cy.get('#add-to-cart-button').click();
 
-    // FAZER MAIS UMA ADIÇÃO AO CARRINHO (mudar item)
-    cy.log('Fazer mais uma adição ao carrinho.');
+    // FAZ MAIS UMA ADIÇÃO NO CARRINHO
+    cy.log('Faz mais uma adição no carrinho.');
     cy.get('#twotabsearchtextbox').type('Capa de celular {enter}');
     cy.get('.s-main-slot .s-result-item[data-component-type="s-search-result"]').first().find('h2 a').click();
-    cy.get('#add-to-cart-button').click({ force: true });
+    cy.get('#add-to-cart-button').click();
 
     // IR PARA O CARRINHO
     cy.log('Ir ao carrinho.');
     cy.get('#sw-gtc > .a-button-inner > .a-button-text').click();
 
     // MUDAR A QUANTIDADE DE UM DOS ITENS
-    cy.log('Mudar a quantia de um dos itens.');
+    cy.log('Mudar a quantidade de um dos itens.');
     cy.get('#a-autoid-0-announce').click();
-    cy.get('#quantity_1').click();
+    cy.get('#quantity_3').click();
 
-    // AUTENTICAÇÃO
+    // REALIZAR LOGIN NA AMAZON
     cy.log('Realizar login na Amazon.');
     cy.get('#nav-link-accountList').click();
     cy.get('#ap_email').type(email);
@@ -42,7 +42,7 @@ describe("Teste compra na Amazon", () => {
     cy.get('body').then(($body) => {
       if ($body.find('button:contains("Agora não")').length > 0) {
         cy.log('Clicar em "Agora não".');
-        cy.get('button:contains("Agora não")').click();
+        cy.get('#ap-account-fixup-phone-skip-link').click();
       }
     });
 
@@ -50,23 +50,25 @@ describe("Teste compra na Amazon", () => {
     cy.log('Fechar Pedido.');
     cy.get('#sc-buy-box-ptc-button > .a-button-inner > .a-button-input').click();
 
-    // Verificar se o botão "Enviar para este endereço" está visível
-    cy.get('#shipToThisAddressButton').then(($button) => {
-      if ($button.is(':visible')) {
+    // Tentar clicar no botão "Enviar para este endereço" (opcional) // tá dando erro aq, qnd n tá salvo
+    cy.get('#shipToThisAddressButton', { timeout: 5000, failOnTimeout: false }).then(($button) => {
+      if ($button.length > 0 && $button.is(':visible')) {
         cy.log('Botão "Enviar para este endereço" encontrado. Clicando diretamente.');
-        cy.get('#shipToThisAddressButton').click();
+        cy.wrap($button).click();
       } else {
-        cy.log('Preencher o endereço.');
-        cy.get('#address-ui-widgets-enterAddressPhoneNumber', { timeout: 10000 }).type('11987654321');
-        cy.get('#address-ui-widgets-enterAddressPostalCode', { timeout: 10000 }).type('50670-902');
+        cy.log('Botão "Enviar para este endereço" não encontrado ou não visível. Preenchendo o endereço.');
+
+        // Preencher o endereço
+        cy.get('#address-ui-widgets-enterAddressPhoneNumber').type('11987654321');
+        cy.get('#address-ui-widgets-enterAddressPostalCode').type('50670-902');
 
         // Clicar na barra de endereço sem escrever nada para auto-preencher
-        cy.get('#address-ui-widgets-streetName', { timeout: 20000 }).should('be.visible').click();
+        cy.get('#address-ui-widgets-streetName').should('be.visible').click();
 
         // Aguardar o preenchimento automático do restante do formulário de endereço
         cy.wait(3000);
-        cy.get('#address-ui-widgets-streetName', { timeout: 10000 }).should('have.value', 'Avenida Caxangá^ 3841');
-        cy.get('#address-ui-widgets-buildingNumber', { timeout: 10000 }).type('3841');
+        cy.get('#address-ui-widgets-streetName').should('have.value', 'Avenida Caxangá^ 3841');
+        cy.get('#address-ui-widgets-buildingNumber').type('3841');
         cy.get('.a-button-inner > #address-ui-widgets-form-submit-button-announce').click({ force: true });
       }
     });
